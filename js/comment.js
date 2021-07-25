f5bd67a9b09ccc0bf491f33a401658c786b36e7b
11h00 <tpjdog@gmail.com>
2021-07-25 22:41:57 +0200

    added interval of count of loop refresh

--- js/comment.js
+++ js/comment.js
@@ -8,101 +8,90 @@ var oldcount = 0
 var uissety = false
 var sonHD = 'true'
 var photo
+
+let loop_count = 0;
 let loop_start = 51.632;
 let loop_end = 53.010 - loop_start
 
-function soundtest(){
-	if (oldcount < photo){
+function soundtest() {
+	if (oldcount < photo) {
 		UPsound.cloneNode(true).play()
 	}
-	if (oldcount > photo){
+	if (oldcount > photo) {
 		DOWNsound.cloneNode(true).play()
 	}
 }
-function milestonztest(){
-	switch(photo.toString().length){
-case 1:
-	var milestonz = photo
-	milestonz += 1
-break;
-case 2:
-	var milestonz = parseInt((Math.floor(photo / 10)) + "0")
-	milestonz += 10
-break;
-case 3:
-	var milestonz = parseInt((Math.floor(photo / 100)) + "00")
-	milestonz += 100
-break;
-case 4:
-	var milestonz = parseInt((Math.floor(photo / 1000)) + "000")
-	milestonz += 1000
-break;
-case 5:
-	var milestonz = parseInt((Math.floor(photo / 10000)) + "0000")
-	milestonz += 10000
-break;
-case 6:
-	var milestonz = parseInt((Math.floor(photo / 100000)) + "00000")
-	milestonz += 100000
-break;
-case 7:
-	var milestonz = parseInt((Math.floor(photo / 1000000)) + "000000")
-	milestonz += 1000000
-break;
-case 8:
-	var milestonz = parseInt((Math.floor(photo / 10000000)) + "0000000")
-	milestonz += 10000000
-break;
-case 9:
-	var milestonz = parseInt((Math.floor(photo / 100000000)) + "00000000")
-	milestonz += 100000000
-break;
+function milestonztest() {
+	switch (photo.toString().length) {
+		case 1:
+			var milestonz = photo
+			milestonz += 1
+			break;
+		case 2:
+			var milestonz = parseInt((Math.floor(photo / 10)) + "0")
+			milestonz += 10
+			break;
+		case 3:
+			var milestonz = parseInt((Math.floor(photo / 100)) + "00")
+			milestonz += 100
+			break;
+		case 4:
+			var milestonz = parseInt((Math.floor(photo / 1000)) + "000")
+			milestonz += 1000
+			break;
+		case 5:
+			var milestonz = parseInt((Math.floor(photo / 10000)) + "0000")
+			milestonz += 10000
+			break;
+		case 6:
+			var milestonz = parseInt((Math.floor(photo / 100000)) + "00000")
+			milestonz += 100000
+			break;
+		case 7:
+			var milestonz = parseInt((Math.floor(photo / 1000000)) + "000000")
+			milestonz += 1000000
+			break;
+		case 8:
+			var milestonz = parseInt((Math.floor(photo / 10000000)) + "0000000")
+			milestonz += 10000000
+			break;
+		case 9:
+			var milestonz = parseInt((Math.floor(photo / 100000000)) + "00000000")
+			milestonz += 100000000
+			break;
 	}
-	
-	if (milestonz == 100000000){
-document.getElementById('milestona').innerHTML = 'before ' + milestonz + " ONG"
-	}else{
-document.getElementById('milestona').innerHTML = 'before ' + milestonz;
+
+	if (milestonz == 100000000) {
+		document.getElementById('milestona').innerHTML = 'before ' + milestonz + " ONG"
+	} else {
+		document.getElementById('milestona').innerHTML = 'before ' + milestonz;
 	}
-odometer5.innerHTML = milestonz - photo
+	odometer5.innerHTML = milestonz - photo
 }
-function sonsetting(){
-if (sonHD == true){
-	document.getElementById("soundstting").innerText = "sound : OFF"
-	document.cookie = "son=false;expires=Thu, 18 Dec 2019 12:00:00 UTC"
-	sonHD = false
-	document.getElementById("soundstting").style.background = "#FF0000"	
-}else{
-	document.getElementById("soundstting").innerText = "sound : ON"
-	document.cookie = "son=true;expires=Thu, 18 Dec 2019 12:00:00 UTC"
-	document.getElementById("soundstting").style.background = "#00FF00"	
-	sonHD = true
-}
-}
-CULER.onreadystatechange = function(event) {
-    if (this.readyState === XMLHttpRequest.DONE) {
-		if (this.status === 200) {
-			degar = JSON.parse(CULER.responseText);
-			photo = degar.graphql.user.edge_felix_video_timeline.count;
-			soundtest()
-			odometer.innerHTML = photo;
-			milestonztest()
-			document.getElementById("coconu").innerHTML = "mathias777";
-        }
-    }
-}
-function tg() {        
-	oldcount = photo
-	setTimeout(function () {
-		CULER.open('GET', "/matashi!/" + useuname);
-		CULER.setRequestHeader("accept", "application/json")
-		CULER.send(null);
-		tg(); 
-			if (uissety == false){
-				updatecount = 5000		
-				uissety = true
-			}
-	}, updatecount)
+function sonsetting() {
+	if (sonHD == true) {
+		document.getElementById("soundstting").innerText = "sound : OFF"
+		document.cookie = "son=false;expires=Thu, 18 Dec 2019 12:00:00 UTC"
+		sonHD = false
+		document.getElementById("soundstting").style.background = "#FF0000"
+	} else {
+		document.getElementById("soundstting").innerText = "sound : ON"
+		document.cookie = "son=true;expires=Thu, 18 Dec 2019 12:00:00 UTC"
+		document.getElementById("soundstting").style.background = "#00FF00"
+		sonHD = true
+	}
 }
 
-tg();  
+setInterval(() => {
+	oldcount = loop_count
+	loop_count++
+	photo = loop_count;
+	soundtest()
+	odometer.innerHTML = photo;
+	milestonztest()
+	document.getElementById("coconu").innerHTML = "mathias777";
+}, 5000);
+
+setInterval(() => {
+	
+})
